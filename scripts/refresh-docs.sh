b8d19b6c7bfc997cfef90a3f9770ff14905d71d7
Brendan Kidwell <snarf@glump.net>
2012-01-29 01:31:11 -0500

    adding pandoc '--standalone' option to refresh-docs.sh

--- scripts/refresh-docs.sh
+++ scripts/refresh-docs.sh
@@ -5,8 +5,8 @@ d=`readlink -f $d/..`
 
 pushd $d >/dev/null
 
-pandoc README.md>README.html
-pandoc doc/options.md>doc/options.html
-pandoc doc/pandoc_markdown.md>doc/pandoc_markdown.html
+pandoc --standalone README.md>README.html
+pandoc --standalone doc/options.md>doc/options.html
+pandoc --standalone doc/pandoc_markdown.md>doc/pandoc_markdown.html
 
 popd >/dev/null
